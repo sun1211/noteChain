912e5edd339cc91fb020dce173334297c439897b
sun1211 <ngoctam121191@yahoo.com>
2018-10-25 15:36:04 +0700

    add lesson 1, create notechain to run, deploy Dapp with contract

---
+++ lesson-1/frontend/src/components/App/App.jsx
@@ -0,0 +1,17 @@
+// React core
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      test app demo dkm bon lua tinh nay
+
+      </div>
+    );
+  }
+
+}
+
+export default App;
